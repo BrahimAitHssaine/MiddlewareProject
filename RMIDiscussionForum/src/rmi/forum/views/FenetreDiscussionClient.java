8c1f78fd30f7f9709246df992769054c0c6abd1e
fadoua <fadoua@192.168.2.108>
2014-10-16 11:05:25 +0200

    commit1

--- RMIDiscussionForum/src/rmi/forum/views/FenetreDiscussionClient.java
+++ RMIDiscussionForum/src/rmi/forum/views/FenetreDiscussionClient.java
@@ -2,7 +2,13 @@ package rmi.forum.views;
 
 import java.awt.HeadlessException;
 
+import javax.swing.BoundedRangeModel;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class FenetreDiscussionClient extends JFrame{
 
@@ -12,7 +18,36 @@ public class FenetreDiscussionClient extends JFrame{
 	private static final long serialVersionUID = 1L;
 	public FenetreDiscussionClient(String title) throws HeadlessException {
 		super();
+		JFrame maFenetre1 = new JFrame();
+		JPanel maPanel = new JPanel();
+		JScrollBar scrollBar = new JScrollBar(JScrollBar.HORIZONTAL);
+	
+		
+		maFenetre1.setTitle(title);
+		maFenetre1.setSize(300, 300);
+		maFenetre1.setResizable(false);			
+		maFenetre1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		maFenetre1.setLocationRelativeTo(null);
+		
+		JTextField input= new JTextField("taper votre message",25);
+		JTextArea Afficheur=new JTextArea(5, 20) ;
+		Afficheur.setEditable(false);
+		BoundedRangeModel brm = input.getHorizontalVisibility();
+		scrollBar.setModel(brm);
+		
+		JButton send = new JButton("Send");//The JButton name.
+	
+		maPanel.add(Afficheur);
+		maPanel.add(input);
+		maPanel.add(scrollBar);
+		maPanel.add(send);
+		maFenetre1.add(maPanel);
+	
+		maFenetre1.setVisible(true);
+		
 	}
+	
+
 	
 	
 }
--- RMIDiscussionForum/src/rmi/forum/views/FenetreSujetClient.java
+++ RMIDiscussionForum/src/rmi/forum/views/FenetreSujetClient.java
@@ -1,18 +1,96 @@
 package rmi.forum.views;
 
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import rmi.forum.interfaces.InterfaceServeurForum;
+import rmi.forum.interfaces.InterfaceSujetDiscussion;
+
+
 
 public class FenetreSujetClient extends JFrame{
 
 	/**
 	 * 
 	 */
+	
+
 	private static final long serialVersionUID = 1L;
 	public FenetreSujetClient(String title) throws HeadlessException {
+		
+	}
+	static JPanel maPanel = new JPanel();
+	private InterfaceSujetDiscussion sujetDiscussion;
+	
+	public FenetreSujetClient() {
 		super();
+		setTitle("FenetreSujetClient ");
+		setSize(400, 300);
+		setResizable(false);				
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		JButton Button1 = new JButton("Sport");//The JButton name.
+		maPanel.add(Button1);
+		JButton Button2 = new JButton("Musique");//The JButton name.
+		maPanel.add(Button2);
+		JButton Button3 = new JButton("Cinema");//The JButton name.
+		maPanel.add(Button3);
+		
+		Button1.addActionListener(new action("Sport")) ;
+		Button2.addActionListener(new action("Musique")) ;
+		Button3.addActionListener(new action("Cinema")) ;
+
 	}
 	
+	class action implements ActionListener
+	{
+		
+		String sujet;
+		public action(String s)
+		{
+			sujet=s;
+		//	sujetDiscussion.inscription(S);
+			System.out.println("je veux faire l'inscription de mon afficheur au sujet x ");
+		}
+		@Override
+		public void actionPerformed(ActionEvent v) {
+			// TODO Auto-generated method stub
+			FenetreDiscussionClient f = new FenetreDiscussionClient(sujet);
+		}
+	}
 	
-	
+	public static void main(String[] args) {
+		// TODO 1. Instancier une JFrame
+		FenetreSujetClient maFenetre2 = new FenetreSujetClient();
+		// TODO 7. Afficher la JFrame
+		maFenetre2.add(maPanel);
+		maFenetre2.setVisible(true);
+		if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+        try {
+            String name = "Server";
+            Registry registry = LocateRegistry.getRegistry(args[0]);
+            InterfaceServeurForum serverforum = (InterfaceServeurForum) registry.lookup(name);
+          
+        } catch (Exception e) {
+            System.err.println("ComputePi exception:");
+            e.printStackTrace();
+        }
+  
+		
+	}
+
 }
+
+	
+	
+
