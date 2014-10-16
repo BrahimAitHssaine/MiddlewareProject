f2661a428a62045c56bb1486402a9b730fb178d0
gyann972 <grego.yannis@gmail.com>
2014-10-16 14:35:09 +0200

    Ajout SujetDiscussion : Implémentation sans intégrer la synchronie.

---
+++ RMIDiscussionForum/src/rmi/forum/core/SujetDiscussion.java
@@ -0,0 +1,67 @@
+package rmi.forum.core;
+
+import java.rmi.RemoteException;
+import java.util.LinkedList;
+import java.util.List;
+
+import rmi.forum.interfaces.InterfaceAffichageClient;
+import rmi.forum.interfaces.InterfaceSujetDiscussion;
+
+public class SujetDiscussion  implements InterfaceSujetDiscussion {
+
+	private List<InterfaceAffichageClient> client;
+	private String name;
+	
+	public SujetDiscussion(String name) {
+		// TODO Auto-generated constructor stub
+		this.setClient(new LinkedList<InterfaceAffichageClient>());
+		this.setName(name);  
+	}
+
+	@Override
+	public void inscription(InterfaceAffichageClient c) throws RemoteException {
+		// TODO Auto-generated method stub
+		this.getClient().add(c);
+	}
+
+	@Override
+	public void desInscription(InterfaceAffichageClient c)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		this.getClient().remove(c);
+	}
+
+	@Override
+	public void diffuse(String message) throws RemoteException {
+		// TODO Auto-generated method stub
+		for(InterfaceAffichageClient c : client){
+			
+			c.affiche(message);
+			
+		}
+	}
+
+	public List<InterfaceAffichageClient> getClient() {
+		return client;
+	}
+
+	public void setClient(List<InterfaceAffichageClient> client) {
+		this.client = client;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public boolean estAbonner(InterfaceAffichageClient c)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
