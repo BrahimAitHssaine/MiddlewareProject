2e83a724c0d52b08a0f416a4a6780617c6e097c9
gyann972 <grego.yannis@gmail.com>
2014-10-17 00:45:42 +0200

    Implémentation du ServeurForum : Constructeur + Méthode.

--- RMIDiscussionForum/src/rmi/forum/core/ServeurForum.java
+++ RMIDiscussionForum/src/rmi/forum/core/ServeurForum.java
@@ -2,6 +2,8 @@ package rmi.forum.core;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Hashtable;
+import java.util.Map;
 
 import rmi.forum.interfaces.InterfaceServeurForum;
 import rmi.forum.interfaces.InterfaceSujetDiscussion;
@@ -12,17 +14,26 @@ public class ServeurForum extends UnicastRemoteObject implements InterfaceServeu
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	private final String musique = "Musique";
+	private final String cinema = "Cinema";
+	private final String sport = "Sport";
+	public static Map<String,InterfaceSujetDiscussion> sujet = new Hashtable<String,InterfaceSujetDiscussion>();
+	
 	protected ServeurForum() throws RemoteException {
 		super();
 		// TODO Auto-generated constructor stub
+		sujet.put(musique, new SujetDiscussion(musique));
+		sujet.put(cinema, new SujetDiscussion(cinema));
+		sujet.put(sport, new SujetDiscussion(sport));
 	}
 
 	@Override
 	public InterfaceSujetDiscussion obtientSujet(String titre)
 			throws RemoteException {
 		// TODO Auto-generated method stub
-		return null;
+		try{ return sujet.get(titre);}
+		catch (NullPointerException e) {return null;}
+		
 	}
 
 }
