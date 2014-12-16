681682d1f4c81d71b637fd29072cf509bca5fd08
Cyrille <agnesgnagne@gmail.com>
2014-12-16 18:43:29 +0100

    v1.1

--- RMIDiscussionForum/src/rmi/forum/client/InterfaceSujetClient.java
+++ RMIDiscussionForum/src/rmi/forum/client/InterfaceSujetClient.java
@@ -3,6 +3,5 @@ package rmi.forum.client;
 import java.rmi.RemoteException;
 
 public interface InterfaceSujetClient {
-	public boolean estAbonner(String c, boolean bool)
-			throws RemoteException ;
+	public boolean estAbonner(String c, boolean bool)throws RemoteException ;
 }
